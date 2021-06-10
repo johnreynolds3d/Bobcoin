d56b9d3bf8cd37a20de34c65b326a5bccf664fdf
John <john@johnreynolds3d.com>
2021-06-10 14:00:19 +0800

    edited Makefile

--- test/heap-profiler.sh
+++ test/heap-profiler.sh
@@ -2,13 +2,13 @@
 HEAPPROFILE=test/heapprof HEAPCHECK=draconian ./bin/bobcoin
 
 # run pprof and view results in a web browser
-pprof -http=localhost:8000 ./bin/bobcoin test/heapprof.0001.heap
+#pprof -http=localhost:8000 ./bin/bobcoin test/heapprof.0001.heap
 
 # run pprof to output one line per procedure 
 #pprof --text ./bin/bobcoin test/heapprof.0001.heap 
 
 # run pprof to get graphical output
-#pprof -gv ./bin/bobcoin test/heapprof.0001.heap
+pprof -gv ./bin/bobcoin test/heapprof.0001.heap
 
 # generate PDF report with previous graphical output
 pprof --pdf ./bin/bobcoin test/heapprof.0001.heap > test/heapprof.0001.heap.pdf 
